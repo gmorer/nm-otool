87649fd997ee15d5ddf37b706bfac78b4b6055ea
Guilhem MORER <gmorer@e1r1p2.42.fr>
2017-12-14 19:14:39 +0100

    statr of nm

--- nm_source/inc/nm.h
+++ nm_source/inc/nm.h
@@ -6,11 +6,28 @@
 /*   By: gmorer <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/14 17:19:32 by gmorer            #+#    #+#             */
-/*   Updated: 2017/12/14 17:20:07 by gmorer           ###   ########.fr       */
+/*   Updated: 2017/12/14 18:58:31 by gmorer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef NM_H
 # define NM_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <sys/mman.h>
+
+enum e_rror
+{
+	ARG = 1,
+	OPEN,
+	CLOSE,
+	FSTAT,
+	MMAP
+};
+
+void  error (enum e_rror error) __attribute__((noreturn));
 
 #endif
--- nm_source/src/main.c
+++ nm_source/src/main.c
@@ -6,13 +6,55 @@
 /*   By: gmorer <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/14 17:20:16 by gmorer            #+#    #+#             */
-/*   Updated: 2017/12/14 17:21:05 by gmorer           ###   ########.fr       */
+/*   Updated: 2017/12/14 19:14:26 by gmorer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "nm.h"
 
-int main(int ac, char **argc)
+void error(enum e_rror error)
 {
+	write(2, "nm: ", 4);
+	if (error == ARG)
+		write(2, "usage: nm [FILE]\n", 17);
+	if (error == OPEN)
+		write(2, "cant open the file\n", 19);
+	if (error == CLOSE)
+		write(2, "cant close the file\n", 20);
+	if (error == CLOSE)
+		write(2, "error during fstat\n", 19);
+	if (error == MMAP)
+		write(2, "error during mmap\n", 18);
+	exit(error + 1);
+}
+/*
+int	arch_separator(void *bin)
+{
+
+}
+*/
+int main(int ac, char **av)
+{
+	int			fd;
+	struct stat	metadata;
+	void		*bin;
+
+	if (ac < 2)
+		error(ARG);
+	if ((fd = open(av[1], O_RDONLY)) == -1)
+		error(OPEN);
+	if (fstat(fd, &metadata) == -1)
+		error(FSTAT);
+	if ((bin = mmap(0, (size_t)metadata.st_size, PROT_READ, MAP_PRIVATE,
+					fd, 0)) == MAP_FAILED)
+		error(MMAP);
+	/* code here */
+
+
+	/* end of the code */
+	if (munmap(bin, (size_t)metadata.st_size) == -1)
+		error(MMAP);
+	if (close(fd) == -1)
+		error(CLOSE);
 	return (1);
 }
